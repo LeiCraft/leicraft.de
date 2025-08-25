009def8f9a923a693f290b3b78122dd01e0842fc
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-08-25 16:27:32 +0000

    update

--- src/views/projects/LeiCoinView.vue
+++ src/views/projects/LeiCoinView.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import ProjectCard from '@/components/ProjectCard.vue';
+import ProjectCard from "@/components/ProjectCard.vue";
 </script>
 
-
 <template>
-
 	<ProjectCard title="LeiCoin" img="/assets/img/leicoin-logo.png">
-		<div style="text-align: center;">
-			LeiCoin ist ein modernes Blockchain-Projekt, das durch eine energieeffiziente Proof-of-Stake
-			(PoS)-Technologie, optimierte Datenkompression und eine skalierbare Architektur überzeugt. Es bietet eine
-			hochperformante, dezentrale Plattform mit effizienter Blockspeicherung, um maximale Performance und globale
-			Skalierbarkeit zu gewährleisten. Ziel ist es, eine sichere, effiziente und zukunftssichere Blockchain-Lösung
-			bereitzustellen.
-		</div>
+		LeiCoin ist ein modernes Blockchain-Projekt, das durch eine
+		energieeffiziente Proof-of-Stake (PoS)-Technologie, optimierte
+		Datenkompression und eine skalierbare Architektur überzeugt. Es bietet eine
+		hochperformante, dezentrale Plattform mit effizienter Blockspeicherung, um
+		maximale Performance und globale Skalierbarkeit zu gewährleisten. Ziel ist
+		es, eine sichere, effiziente und zukunftssichere Blockchain-Lösung
+		bereitzustellen.
 	</ProjectCard>
-
-
 </template>
