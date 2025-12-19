f8f2ce1311c0ecec7a6d98fabaec206819ef2def
Linus Fischer <leicraft@leicraftmc.de>
2025-12-19 12:05:43 +0000

    update

---
+++ src/views/projects/LeiOSView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ProjectCard from "@/components/ProjectCard.vue";
+</script>
+
+<template>
+	<ProjectCard title="LeiOS" img="/assets/img/leicoin-logo.png">
+		LeiOS ist eine Debian-basierte Linux-Distribution mit Fokus auf Stabilität, Performance und einer außergewöhnlichen Out-of-Box-Experience.
+		Aufbauend auf Debians legendärer stabiler Basis bietet es felsenfeste Zuverlässigkeit mit sorgfältig abgestimmten Optimierungen für maximale Leistung.
+		Das System kommt vorkonfiguriert mit sinnvollen Standardeinstellungen, essentiellen Entwicklerwerkzeugen wie Git und Build-Essentials sowie einer polierten Desktop-Umgebung.
+		Obwohl für Entwickler optimiert, ist LeiOS so konzipiert, dass es für jeden zugänglich ist, der eine zuverlässige Linux-Erfahrung sucht.
+		Es ist vollständig Open Source, Community-getrieben und Open Source unter der AGPL-3.0-Lizenz.
+	</ProjectCard>
+</template>
